eb523278729d1d4b26a7708a447df58c79ca229f
EWassmann <107703321+EWassmann@users.noreply.github.com>
2022-07-06 16:34:46 -0400

    Add files via upload

---
+++ keyboardctrl.py
@@ -0,0 +1,56 @@
+import serial
+
+arduino = serial.Serial(
+port = '/dev/ttyACM0',
+baudrate = 2000000, #perhaps make this lower need to do research
+bytesize = serial.EIGHTBITS,
+parity = serial.PARITY_NONE,
+stopbits = serial.STOPBITS_ONE,
+timeout = 5,
+xonxoff = False,
+rtscts = False,
+dsrdtr = False,
+writeTimeout = 2
+)
+#----------------Functions---------------------------
+def Left():
+    arduino.write("1".encode()) 
+    global b
+    b = 1
+def Forward():
+    arduino.write("0".encode())
+    global b
+    b = 0
+def Right():
+    arduino.write("2".encode())
+    global b
+    b = 2
+def Stop():
+    arduino.write("4".encode())
+    global b
+    b = 4
+def Back():
+    arduino.write("3".encode())
+    global b
+    b = 3
+def Search():
+    arduino.write("5".encode())
+    global b
+    b = 5
+
+
+#----------------------------------------------------
+while True:
+
+    directions = input()
+    if directions == "w":
+        Forward()
+    if directions == "a":
+        Left()
+    if directions =="d":
+        Right()
+    if directions == "s":
+        Stop()
+    if directions == "ss":
+        Back()    
+
